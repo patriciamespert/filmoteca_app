be885e4b8695788780ca5ec3fbbccd4781e18fc9
Patricia Martínez Espert <pmespert@gmail.com>
2022-09-15 05:11:27 +0200

    interface (xml) film data and film edit

--- app/src/main/java/com/mundoandroid/filmoteca/FilmDataActivity.kt
+++ app/src/main/java/com/mundoandroid/filmoteca/FilmDataActivity.kt
@@ -1,6 +1,7 @@
 package es.ua.eps.filmoteca
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import es.ua.eps.filmoteca.databinding.ActivityAboutBinding
@@ -13,7 +14,7 @@ class FilmDataActivity : AppCompatActivity() {
         binding = ActivityFilmDataBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnWatchMovie.setOnClickListener{startActivity(Intent(this@FilmDataActivity,this::class.java))}
+        binding.btnWatchIMDB.setOnClickListener{startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.imdb.com/search/title/?title=Back+to+the+Future&release_date=,1985-12-31&genres=sci-fi")))}
         binding.btnEditMovie.setOnClickListener{startActivity(Intent(this@FilmDataActivity,FilmEditActivity::class.java))}
         binding.goBackMain.setOnClickListener{startActivity(Intent(this@FilmDataActivity,FilmListActivity::class.java))}
     }
